0f1fda5dfd0724389404f9afbb53c0bab6bc124c
zz070 <2389655715@qq.com>
2020-04-10 09:42:15 +0800

    异常框架

--- A.java
+++ A.java
@@ -1,7 +1,13 @@
 package study;
 
 public class A {
-    public void Az(){
-
+    public void tryPractice(){
+        try {
+            //可能出现异常的语句
+        }catch (Exception e){   //catch (异常类型 异常对象)
+            //处理异常
+        }finally {
+            //善后工作
+        }
     }
 }
