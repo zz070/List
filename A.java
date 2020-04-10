50c01570dd5fa9a6b379466d235fe87d1ef296e8
zz070 <2389655715@qq.com>
2020-04-10 15:38:54 +0800

    异常框架

--- A.java
+++ A.java
@@ -1,13 +1,20 @@
 package study;
 
+import java.util.Scanner;
+
 public class A {
-    public void tryPractice(){
-        try {
-            //可能出现异常的语句
-        }catch (Exception e){   //catch (异常类型 异常对象)
-            //处理异常
-        }finally {
-            //善后工作
+    public void tryPractice() {
+        try (Scanner sc = new Scanner(System.in)) {
+            int num = sc.nextInt();
+            System.out.println("num = " + num);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+    public int divide(int x, int y) throws ArithmeticException {
+        if (y == 0) {
+            throw new ArithmeticException("除 0 异常");
+        }
+        return x / y;
     }
 }
